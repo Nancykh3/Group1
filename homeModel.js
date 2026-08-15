5a762e442efa414da108996b916947994690ab66
shahedbondoq8-jpg <shahedbondoq8@gmail.com>
2026-08-15 23:22:59 +0300

    Delete homeModel.js

--- homeModel.js
+++
@@ -1,7 +0,0 @@
-const homeData = {
-    title: "Fast & Affordable Recipe Platform",
-    description:
-        "Discover quick, delicious, and budget-friendly recipes made especially for students."
-};
-
-module.exports = homeData;
