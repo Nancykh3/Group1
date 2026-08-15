cbdf9634cf480bcf6e6112069a45622552b81fab
shahedbondoq8-jpg <shahedbondoq8@gmail.com>
2026-08-15 23:21:51 +0300

    Delete featureRoutes.js

--- featureRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const featureController = require("../controllers/featureController");
-
-router.get("/features", featureController.showFeatures);
-
-module.exports = router;
